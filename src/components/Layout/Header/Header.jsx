572f624a654e0809c7524c0041f99794b87e7077
Ziad Eleraky <ziadeleraky1@gmail.com>
2023-04-30 20:52:46 +0300

    Initial commit

---
+++ src/components/Layout/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from "./Header.module.css";
+import mealsImg from "../../../assets/imgs/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Elcheif</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImg} alt="A table full of meals" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
